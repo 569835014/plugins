ca39a0f7497e2b7e6dafd9cfcbb98664c57acd99
minzhang <569835014@qq.com>
2018-05-02 23:37:56 +0800

    添加util类

---
+++ libraries/util/index.js
@@ -0,0 +1,6 @@
+import * as Dom from './dom'
+import URL from './url'
+export  {
+  Dom,
+  URL
+}
---
+++ libraries/util/url.js
@@ -0,0 +1,16 @@
+const URL = {
+  // 当前页面路径
+  href: window.location.href,
+  // 当前页面查询字符串
+  search: window.location.search,
+  // 查询URL传递的参数值
+  query: function (key) {
+    let reg = new RegExp('(^|&|\\?|#)' + key + '=([^&]*)(&|\x24)', '')
+    let match = this.search.substr(1).match(reg)
+    if (match) {
+      return match[2]
+    }
+    return ''
+  }
+}
+export default URL
