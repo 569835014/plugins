3ce0abd15180e169f4ad30e549a25ec0e3d34262
Yakima <cleveryun@163.com>
2018-05-04 14:35:04 +0800

    npm run build命令打包库文件支持同时打包压缩版和未压缩版，打包文件纳入git版本控制，方便他们直接取用库文件

--- build/build-uncompress.js
+++ build/build-uncompress.js
@@ -1,7 +1,7 @@
 'use strict'
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = 'public'
 
 const ora = require('ora')
 const rm = require('rimraf')
@@ -9,7 +9,7 @@ const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
-const webpackConfig = require('./webpack.docs.prod.conf')
+const webpackConfig = require('./webpack.build.uncompress.conf')
 
 const spinner = ora('building for production...')
 spinner.start()
