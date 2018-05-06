104cee468fb3c2d0a3c330c6a1bdd0069a5a33d0
Yakima <cleveryun@163.com>
2018-05-06 13:30:28 +0800

    [fix] 更正package.json中docs:dev值中的文件名

--- libraries/util/typeOf.js
+++
@@ -1,8 +0,0 @@
-/**
- * 判断对象类型
- * @param  {any} input    目标对象
- * @return {string}       目标对象的类型：'array', 'object', 'function', 'null', 'undefined', 'string', 'number'
- */
-export default function (input) {
-  return ({}).toString.call(input).slice(8, -1).toLowerCase()
-}
