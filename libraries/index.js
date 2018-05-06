f512d0646d54cd6bc48fc6180e038bea44a7daa2
Yakima <cleveryun@163.com>
2018-05-06 13:04:59 +0800

    gst

--- libraries/index.js
+++ libraries/index.js
@@ -3,12 +3,14 @@ import Session from './session/Session'
 import * as Util from './util/index'
 const {hasClass, getData, addClass, prefixStyle, absolutePosition, assertionDom, getStyle,} = Util.Dom;
 const {random}=Util.Tool
-const URL = Util.URL;
 const wePlugins = {
   Subscribe,
   Session,
   Util
 };
+const URL = Util.URL;
+const typeOf = Util.typeOf;
+const hasValue = Util.hasValue;
 
 export {
   Subscribe,
@@ -21,8 +23,9 @@ export {
   assertionDom,
   getStyle,
   random,
-  URL
+  URL,
+  typeOf,
+  hasValue
 }
 window.wePlugins = wePlugins
 export default wePlugins
-
